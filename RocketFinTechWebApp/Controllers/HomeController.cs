8832c1ecbb7c9042cec52f1db87d00646a1fde7d
iaguilar313 <iaguilar@umich.edu>
2017-10-26 13:52:55 -0400

    WebApp from midterm

---
+++ RocketFinTechWebApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RocketFinTechWebApp.Models;
+
+namespace RocketFinTechWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Qualify()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Sorry()
+        {
+            ViewBag.Message = "You can try Sears";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProcessForm(UserData UserInfo)
+        {
+            ViewBag.ClientName = UserInfo.Username;
+            ViewBag.Income = UserInfo.Income;
+            ViewBag.Score = UserInfo.Score;
+
+
+            if (UserInfo.Score == "550-649")
+
+                return View("Sorry");
+
+            else
+                return View ("Checkout");
+        }
+    }
+}
---
+++ RocketFinTechWebApp/Models/UserData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RocketFinTechWebApp.Models
+{
+    public class UserData
+    {
+        private string username;
+        private string phonenumber;
+        private string email;
+        private string income;
+        private string score;
+
+        public string Username
+        {
+            set { username = value; }
+            get { return username; }
+        }
+
+        public string Phonenumber
+        {
+            set { phonenumber = value; }
+            get { return phonenumber; }
+        }
+
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+
+        }
+
+        public string Income
+        {
+            set { income = value; }
+            get { return income; }
+        }
+
+        public string Score
+        {
+            set { score = value; }
+            get { return score; }
+        }
+
+        public UserData()
+        {
+            Username = "";
+            Phonenumber = "";
+            Email = "";
+            Income = "";
+            Score = "";
+        }
+        public UserData(string Dun , string Dp, string De, string Di, string Ds)
+        {
+            Username = Dun;
+            Phonenumber = Dp;
+            Email = De;
+            Income = Di;
+            Score = Ds;
+        }
+
+    }
+}
